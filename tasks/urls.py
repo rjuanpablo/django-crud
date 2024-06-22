0f9d557d934cc43536cf84db85c55e8d6b79a211
Juan Pablo <58188157+rjuanpablo@users.noreply.github.com>
2024-06-22 14:07:02 -0300

    feat: login

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
+
+    #path('profile/', views.profile, name='profile'),
+    #path('settings/', views.settings, name='settings'),
+    #path('password_change/', views.password_change, name='password_change'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,28 +1,83 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from django.db import IntegrityError
 
 # Create your views here.
 
+
 def signup(request):
     if request.method == 'GET':
-        return render(request, 'signup.html',{
+        return render(request, 'signup.html', {
             'form': UserCreationForm(),
         })
     else:
-        if request.POST['password1'] == request.POST['password2'] and request.POST['password1']!= '' and request.POST['username']!= '':
+        if request.POST['password1'] == request.POST['password2'] and request.POST['password1'] != '' and request.POST['username'] != '':
             try:
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse('successful signup! You can now log in.')
-            except:
-                return HttpResponse('Signup failed! Username already exists.')
-            
-        return HttpResponse('Signup failed! Passwords do not match or username is empty.')
-    
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm(),
+                    'error': 'Username already exists.',
+                })
+
+        return render(request, 'signup.html', {
+            'form': UserCreationForm(),
+            'error': 'Passwords do not match.',
+        })
+
+
 def index(request):
     return render(request, 'index.html')
 
+
 def home(request):
     return render(request, 'home.html')
+
+def tasks(request):
+    return render(request, 'tasks.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm(),
+        })
+    else:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('tasks')
+        else:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm(),
+                'error': 'Invalid username or password.',
+            })
+
+
+def signin2(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('tasks')
+        else:
+            return render(request, 'signin.html', {
+                'error': 'Invalid username or password.',
+            })
+    else:
+        return render(request, 'signin.html')
