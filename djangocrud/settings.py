52e60a32fdcc3d705956f2dd5fb0d9c21582528f
Juan Pablo <58188157+rjuanpablo@users.noreply.github.com>
2024-06-23 14:03:16 -0300

    fix: settings.py

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -84,15 +84,10 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': { dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
-        conn_max_age=600
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
     )
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
