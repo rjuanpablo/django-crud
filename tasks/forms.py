f25bc04456d738988d49e838f379141e537aa6a9
Juan Pablo <58188157+rjuanpablo@users.noreply.github.com>
2024-06-23 11:49:27 -0300

    feat: include bootstrap

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
 from .models import Task
+from django import forms
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter task title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter task description'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
